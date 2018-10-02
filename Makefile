d91bf76b76c380fa80f2bc250a748263cd057b7c
kiraliao1018 <kiraliao1018@gmail.com>
2018-10-02 18:47:42 +0000

    “setup”

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 CC=gcc
 CFLAGS=-lWarn -pedantic
 
+output: irtester.o libmyifttt.a
+	$(CC) irtester.o -L. -lwiringPi -lmyifttt -lcurl -o irtester
+
+
 tester: tester.o libmyifttt.a
 	$(CC) tester.o -L. -lmyifttt -lcurl -o tester
 
@@ -13,5 +17,10 @@ ifttt.o: 	ifttt.c ifttt.h
 tester.o:	tester.c ifttt.h
 	$(CC) $(CFLAGS) -c -ansi $<
 
+irtester.o:	irtester.c ifttt.h
+	$(CC) $(CFLAGS) -c -ansi $<
+
 clean:
 	rm tester *.o
+
+all: tester output
--- irtester.c
+++ irtester.c
@@ -6,11 +6,22 @@ int main(int argc, char *argv[])
   int i;
   wiringPiSetup () ;
   pinMode(0, INPUT);
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
   while(1) {
-    printf("Waiting for reset\n");
-    while(digitalRead(0) == 1);
-    printf("Waiting for event\n");
-    while(digitalRead(0) == 0);
+    printf("waitting for reset\n");
+    while (digitalRead(0)==1){
+    printf("Got a 1\n");
+    digitalWrite(2,HIGH);
+    digitalWrite(1,LOW);
+    ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/1018", "kkkiii", "rrr", "aaa");
+}
+   printf("waitting for event\n");
+    if(digitalRead(0) == 0){
+    printf("Got a 0\n");
+    digitalWrite(2,LOW);
+    digitalWrite(1,HIGH);delay(200);
+    digitalWrite(1,LOW);}
     printf("Alarm\n");
   }
   /*NOTREACHED*/
